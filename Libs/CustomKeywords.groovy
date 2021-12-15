c04827291cd51ef175b9e2be26c367ad03b4e883
RaizaKurniawan <raizakurniawan@gmail.com>
2021-12-15 11:10:14 +0700

    fromupload
    due git and katalon wont connect

---
+++ Libs/CustomKeywords.groovy
@@ -0,0 +1,14 @@
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+
+import java.lang.String
+
+
+
+def static "swiping.scrollListToElementWithText"(
+    	String elementText	) {
+    (new swiping()).scrollListToElementWithText(
+        	elementText)
+}
